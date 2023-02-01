d009f91b0d70a947b39fb0bf8ce690d3b8ef5519
Zander Martineau <hi@zander.wtf>
2023-02-01 06:52:18 +0000

    update description

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,13 +2,15 @@ module.exports = {
   meta: {
     title: 'Code Notes',
     showTitleInSidebar: false,
-    description: 'Notes on code, TILs, my digital garden. By Zander Martineau',
+    description:
+      'Notes on code, TILs, snippets - my digital garden. By Zander Martineau',
     lang: 'en',
     siteUrl: 'https://notez.zander.wtf/',
   },
   feed: {
     // used in feed.xml.njk
-    subtitle: 'Notes on code, TILs, my digital garden. By Zander Martineau',
+    subtitle:
+      'Notes on code, TILs, snippets - my digital garden. By Zander Martineau',
     filename: 'atom.xml',
     path: '/atom.xml',
     id: 'https://example.com/',
